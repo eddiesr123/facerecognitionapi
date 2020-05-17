e3f3b47f2b97d9def1a365b33e434ad6bf2712e7
eddiesr123 <eddiesr1234@gmail.com>
2020-05-17 02:19:51 -0700

    updated node ES module imports mjs

--- index.mjs
+++ index.mjs
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import db from './database.js';
+import db from './database.mjs';
 import bcrypt from 'bcrypt-nodejs';
-import { handleRegister } from './controllers/register.js';
-import { handleSignin } from './controllers/signin.js';
-import { handleProfileGet } from './controllers/profile.js';
-import { handleImageEntries, handleApiCall } from './controllers/image.js';
+import { handleRegister } from './controllers/register.mjs';
+import { handleSignin } from './controllers/signin.mjs';
+import { handleProfileGet } from './controllers/profile.mjs';
+import { handleImageEntries, handleApiCall } from './controllers/image.mjs';
 
 const app = express();
 
